09794c514c4015ef31b2d8a0c734db72945ead56
gary909 <garywhite61@hotmail.com>
2022-11-15 12:56:02 +0100

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function elevator(left, right, call) {
+    // code on! :)
+}
+
+console.log(elevator(0, 1, 0)); // 'left'
+console.log(elevator(0, 1, 1)); // 'right'
+console.log(elevator(0, 1, 0)); // 'right'
+console.log(elevator(0, 1, 2)); // 'right'
+console.log(elevator(0, 0, 0)); // 'right'
+console.log(elevator(0, 2, 1)); // 'right'
